eaa8606a56b27394718ac7472bdbbbf94c25ef30
a.melostnoy <a.melostnoy@gmail.com>
2019-09-17 19:05:15 +0300

    added test for check search mechanism

--- src/test/java/runner/MainRunner.java
+++ src/test/java/runner/MainRunner.java
@@ -1,25 +1,15 @@
 package runner;
 
-import com.cucumber.listener.Reporter;
 import cucumber.api.CucumberOptions;
-import org.testng.annotations.AfterClass;
 import tests.BaseTest;
-
-import java.io.File;
-import java.io.IOException;
 
 @CucumberOptions(
         features = "src/test/java/features"
         ,glue={"steps"}
-        ,plugin = { "pretty", "html:target/cucumber", "json:target/cucumber.json",
-        "com.cucumber.listener.ExtentCucumberFormatter:target/extent-report/report.html" }
+        ,plugin = { "pretty", "html:target/cucumber", "json:target/cucumber.json" }
         ,monochrome = false
 )
 
 public class MainRunner extends BaseTest {
-    @AfterClass
-    public static void writeExtentReport() throws IOException {
-        //Reporter.loadXMLConfig(new File(System.getProperty("user.dir") + "\\src\\main\\java\\utils\\ReportsConfig.xml"));
-    }
 
 }
