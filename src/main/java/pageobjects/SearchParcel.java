8dab5534ec8501f43e18be95cbb5aba919e51c47
evgeny <petrpug@gmail.com>
2019-09-18 15:59:39 +0300

    var2

---
+++ src/main/java/pageobjects/SearchParcel.java
@@ -0,0 +1,27 @@
+package pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SearchParcel extends BasePage {
+
+    @FindBy(xpath ="//*[text()='Помощь']")
+    private WebElement helpBtn;
+    @FindBy(xpath ="//*[text()='Помощь']//..//*[text()='Где посылка?']")
+    private WebElement whereParcel;
+
+    public SearchParcel(WebDriver driver) {
+        super (driver);
+        this.driver = driver;
+    }
+    public SearchParcel helpButton(){
+        helpBtn.click();
+        return this;
+    }
+
+    public SearchParcel whereParcelButton() {
+        whereParcel.click();
+        return this;
+    }
+}
