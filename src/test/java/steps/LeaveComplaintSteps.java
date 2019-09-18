80f9d9c9d38989e9be6c55c48891cb4073a8349a
Elena <elena.d.makeeva@gmail.com>
2019-09-18 16:44:12 +0300

    Add function, alteration

--- src/test/java/steps/LeaveComplaintSteps.java
+++ src/test/java/steps/LeaveComplaintSteps.java
@@ -3,32 +3,31 @@ package steps;
 import cucumber.api.java.ru.Допустим;
 import cucumber.api.java.ru.Когда;
 import cucumber.api.java.ru.Тогда;
-import org.openqa.selenium.support.PageFactory;
 import pageobjects.ComplaintPage;
-import pageobjects.HomePage;
+import pageobjects.MainPage;
 import pageobjects.ServicePage;
 import tests.BaseTest;
 
 public class LeaveComplaintSteps extends BaseTest {
 
-    HomePage homePage;
+    MainPage mainPage;
     ServicePage servicePage;
     ComplaintPage complaintPage;
 
     @Допустим("^пользователь находится на главной странице$")
     public void пользователь_находится_на_главной_странице() throws Throwable {
         driver.get("https://magazintrav.ru/");
-        homePage = PageFactory.initElements(driver, HomePage.class);
+        mainPage = new MainPage(driver);
     }
 
     @Когда("^нажимает на кнопку Помощь$")
     public void нажимает_на_кнопку_Помощь() throws Throwable {
-       homePage.clickMenuHelp();
+       mainPage.clickMenuHelp();
     }
 
     @Когда("^выбирает пункт службы контроля качества$")
     public void выбирает_пункт_службы_контроля_качества() throws Throwable {
-        servicePage = homePage.clickService();
+        servicePage = mainPage.clickService();
     }
 
     @Тогда("^пользователь нажимает на кнопку оставить жалобу$")
---
+++ src/test/java/steps/StepsWholesaleSite.java
@@ -0,0 +1,33 @@
+package steps;
+
+import cucumber.api.java.ru.Допустим;
+import cucumber.api.java.ru.Когда;
+import cucumber.api.java.ru.Тогда;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import pageobjects.MainPage;
+import pageobjects.WholesalePage;
+import tests.BaseTest;
+
+import java.util.ArrayList;
+
+public class StepsWholesaleSite extends BaseTest {
+
+    MainPage mainPage = new MainPage(driver);
+    WholesalePage wholesalePage;
+
+    @Когда("^пользователь нажимает на кнопку Оптовикам$")
+    public void пользователь_нажимает_на_кнопку_Оптовикам() throws Throwable {
+       wholesalePage = mainPage.clickWholesaleBtn();
+    }
+
+    @Тогда("^пользователь попадает на внешний ресурс$")
+    public void пользователь_попадает_на_внешний_ресурс() throws Throwable {
+        ArrayList<String> tabs = new ArrayList<> (driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+        String email = wholesalePage.getEmailOpt().getText();
+        Assert.assertEquals(email, "opt@magazintrav.ru");
+    }
+
+}
